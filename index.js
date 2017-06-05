146b16e5acea81a8a4806f784bfeeabb74590c72
Riccardo Coppola <Riccardo.Coppola@thetrainline.com>
2017-06-06 00:06:00 +0200

    Prettier

--- index.js
+++ index.js
@@ -1,41 +1,37 @@
 #!/usr/bin/env node
 
-const fetch = require('node-fetch')
-const swaggerToFlowTypes = require('./lib/swagger_to_flow_types')
+const fetch = require("node-fetch");
+const swaggerToFlowTypes = require("./lib/swagger_to_flow_types");
 
 if (!Object.entries) {
-  require('object.entries').shim()
+  require("object.entries").shim();
 }
 
-const argv = require('yargs')
-  .usage('Usage: $0 --url url_of_swagger.json')
-  .describe('url', 'URL for the swagger json file')
-  .describe('transformProperty', 'transforms a property name')
-  .choices('transformProperty', ['normal', 'firstCaseLower'])
-  .default('transformProperty', 'normal')
-  .demand(['url'])
-  .argv;
-
+const argv = require("yargs")
+  .usage("Usage: $0 --url url_of_swagger.json")
+  .describe("url", "URL for the swagger json file")
+  .describe("transformProperty", "transforms a property name")
+  .choices("transformProperty", ["normal", "firstCaseLower"])
+  .default("transformProperty", "normal")
+  .demand(["url"]).argv;
 
 fetch(argv.url)
-  .then((response) => response.json())
-  .then((json) => {
-    let data = []
+  .then(response => response.json())
+  .then(json => {
+    let data = [];
     if (json.definitions) {
       for (let [key, value] of Object.entries(json.definitions)) {
-        data.push(`export type ${key} = ${processDefinition(key, value)}`)
+        data.push(`export type ${key} = ${processDefinition(key, value)}`);
       }
 
-      console.log(data.join('\n\n'))
-
+      console.log(data.join("\n\n"));
     } else {
-      throw new Error('No swagger definitions to parse')
+      throw new Error("No swagger definitions to parse");
     }
   })
   .catch(e => {
-    console.error(e.message)
-  })
-
+    console.error(e.message);
+  });
 
 /**
  * Process the individual definition
@@ -44,10 +40,15 @@ fetch(argv.url)
  * @return {string}
  */
 function processDefinition(name, data) {
-  if (data.type == 'object') {
-    return `{\n\t${Object.entries(data.properties).map(([key, value]) => `${parsePropertyName(key)}: ${parsePropertyType(value)}`).join(',\n\t')}\n}`
+  if (data.type == "object") {
+    return `{\n\t${Object.entries(data.properties)
+      .map(
+        ([key, value]) =>
+          `${parsePropertyName(key)}: ${parsePropertyType(value)}`
+      )
+      .join(",\n\t")}\n}`;
   } else {
-    throw new Error(`Unable to parse ${data.type} for ${name}`)
+    throw new Error(`Unable to parse ${data.type} for ${name}`);
   }
 }
 
@@ -57,16 +58,16 @@ function processDefinition(name, data) {
  * @return {string}
  */
 function parsePropertyType(type) {
-  if (type.type == 'array') {
+  if (type.type == "array") {
     if (type.items.type) {
-      return `Array<${swaggerToFlowTypes[type.items.type]}>`
-    } else if (type.items['$ref']) {
-      return `Array<${type.items['$ref'].replace('#/definitions/', '')}>`
+      return `Array<${swaggerToFlowTypes[type.items.type]}>`;
+    } else if (type.items["$ref"]) {
+      return `Array<${type.items["$ref"].replace("#/definitions/", "")}>`;
     }
   } else if (!type.type && type["$ref"]) {
-    return type['$ref'].replace('#/definitions/', '')
+    return type["$ref"].replace("#/definitions/", "");
   } else {
-    return swaggerToFlowTypes[type.type]
+    return swaggerToFlowTypes[type.type];
   }
 }
 
@@ -77,11 +78,12 @@ function parsePropertyType(type) {
  */
 function parsePropertyName(name) {
   switch (argv.transformProperty) {
-    case 'firstCaseLower':
-      if (!/[a-z]/.test(name)) { // Doesn't have a single lower case character, probably need to make the entire word lower case
-        return name.toLowerCase()
+    case "firstCaseLower":
+      if (!/[a-z]/.test(name)) {
+        // Doesn't have a single lower case character, probably need to make the entire word lower case
+        return name.toLowerCase();
       } else {
-        return name.charAt(0).toLowerCase() + name.slice(1)
+        return name.charAt(0).toLowerCase() + name.slice(1);
       }
   }
 
