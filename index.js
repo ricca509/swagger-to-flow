9429316d0794431769ff74ce3b63cc37ebf5cb67
Riccardo Coppola <coppola.riccardo@gmail.com>
2017-06-14 11:23:35 +0100

    Creates and resolves dependency graph for types  (#2)
    * First attempt at creating and resolving a tree of dependency
    
    * Changes the way types are printed to the console
    
    * Adds a printDefinitions fn

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ const fetch = require("node-fetch");
 const fs = require("fs");
 const https = require("https");
 const changeCase = require("change-case");
+const { DepGraph } = require("dependency-graph");
 const swaggerToFlowTypes = require("./lib/swagger_to_flow_types");
 
 if (!Object.entries) {
   require("object.entries").shim();
 }
+
+const graph = new DepGraph();
 
 const argv = require("yargs")
   .usage("Usage: $0 -p [path] <options>")
@@ -36,7 +39,8 @@ isUrl.test(path) ? fetchDefinitions(path) : readDefinitions(path);
 function readDefinitions(file) {
   const json = fs.readFileSync(file, "utf-8");
   try {
-    processDefinitions(JSON.parse(json));
+    const definitions = processDefinitions(JSON.parse(json));
+    printDefinitions(definitions);
   } catch (error) {
     console.log(error);
   }
@@ -49,23 +53,29 @@ function fetchDefinitions(url) {
   return fetch(url, options)
     .then(response => response.json())
     .then(processDefinitions)
+    .then(printDefinitions)
     .catch(e => {
       console.error(e.message);
     });
 }
 
 function processDefinitions(json) {
-  let data = ["// @flow"];
+  let data = {};
   if (json.definitions) {
     for (let [key, value] of Object.entries(json.definitions)) {
       const name = getTypeName(key);
-      data.push(`export type ${name} = ${processDefinition(name, value)}`);
+      graph.addNode(name);
+      data[name] = `export type ${name} = ${processDefinition(name, value)}`;
     }
 
-    console.log(data.join("\n\n"));
+    return ["// @flow", ...graph.overallOrder().map(type => data[type])];
   } else {
     throw new Error("No swagger definitions to parse");
   }
+}
+
+function printDefinitions(definitions) {
+  return definitions.map(definition => console.log(definition));
 }
 
 /**
@@ -79,7 +89,7 @@ function processDefinition(name, data) {
     return `{\n\t${Object.entries(data.properties)
       .map(
         ([key, value]) =>
-          `${parsePropertyName(key)}: ${parsePropertyType(value)}`
+          `${parsePropertyName(key)}: ${parsePropertyType(value, name)}`
       )
       .join(",\n\t")}\n}`;
   } else {
@@ -92,18 +102,19 @@ function processDefinition(name, data) {
  * @param type
  * @return {string}
  */
-function parsePropertyType(type) {
+function parsePropertyType(type, key) {
   if (type.type == "array") {
     if (type.items.type) {
       return `Array<${swaggerToFlowTypes[type.items.type]}>`;
     } else if (type.items["$ref"]) {
-      return `Array<${getTypeName(
-        type.items["$ref"].replace("#/definitions/", "")
-      )}>`;
+      const ref = getTypeName(type.items["$ref"].replace("#/definitions/", ""));
+      addGraphDependency(key, ref);
+      return `Array<${ref}>`;
     }
   } else if (!type.type && type["$ref"]) {
-    const ref = type["$ref"].replace("#/definitions/", "");
-    return getTypeName(ref);
+    const ref = getTypeName(type["$ref"].replace("#/definitions/", ""));
+    addGraphDependency(key, ref);
+    return ref;
   } else {
     return swaggerToFlowTypes[type.type];
   }
@@ -128,4 +139,9 @@ function parsePropertyName(name) {
 
 function getTypeName(type) {
   return changeTypeCase ? changeCase.pascalCase(type) : type;
+}
+
+function addGraphDependency(parent, node) {
+  graph.addNode(node);
+  parent !== node && graph.addDependency(parent, node);
 }
